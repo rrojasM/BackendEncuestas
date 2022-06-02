ef1ce0ce900f5ad2b2062f0b085d17e66c681158
Rufino Rojas <rojasru@globalhitss.com>
2022-06-02 18:00:20 -0500

    initial commit backend encuestas

---
+++ src/main/java/com/rrojas/SpringReact/EnscuestaBackend/ProyectoDeEnscuestaBackendConSpringApplication.java
@@ -0,0 +1,14 @@
+package com.rrojas.SpringReact.EnscuestaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDeEnscuestaBackendConSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDeEnscuestaBackendConSpringApplication.class, args);
+		System.out.println("FUNCIONANDO........");
+	}
+
+}
---
+++ src/test/java/com/rrojas/SpringReact/EnscuestaBackend/ProyectoDeEnscuestaBackendConSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrojas.SpringReact.EnscuestaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDeEnscuestaBackendConSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
